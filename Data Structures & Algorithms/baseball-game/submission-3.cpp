ac2aa2789611925ad7a05dec6c98d8a1045402fb
Aditi karunakar Shetty <shettyaditi2006@gmail.com>
2026-09-10 20:03:12 +0530

    Add: baseball-game - submission-3

---
+++ Data Structures & Algorithms/baseball-game/submission-3.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        stack<int>stack;
+        int res=0;
+        for(const string& op: operations){
+            if(op =="+"){
+                int top=stack.top();
+                stack.pop();
+                int newtop= top+stack.top();
+                stack.push(top);
+                stack.push(newtop);
+                res+= newtop;
+            }else if(op =="C"){
+                res-=stack.top();
+                stack.pop();
+            }else if(op=="D"){
+                stack.push(2*stack.top());
+                res+= stack.top();
+
+            }else{
+                stack.push(stoi(op));
+                res+=stack.top();
+            }
+        }
+        return res;
+    }
+};
